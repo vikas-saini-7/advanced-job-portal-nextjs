2cdd8600f8525878c0d3ea07d3a1424fd75c87cd
vikas-saini-7 <vikas289888@gmail.com>
2024-07-24 09:54:29 +0530

    created header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
+import Header from "@/components/common/Header";
+import Image from "next/image";
 
-
-export default async function Home() {
-  
+export default function Home() {
   return (
-    <div>
-      page
-    </div>
+    <main className="">
+      <Header />
+    </main>
   );
 }
